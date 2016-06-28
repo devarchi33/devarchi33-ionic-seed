0f37bfc0df40c82c57d448a6c3a7eedec696a2f3
devarchi33 <devarchi33@gmail.com>
2016-06-28 15:47:47 +0900

    1. 메인뷰 수정. (rounded thumnail)

--- www/js/ctrl/mainCtrl.js
+++ www/js/ctrl/mainCtrl.js
@@ -4,4 +4,13 @@
 angular.module('strt.ctrls.mainCtrl', [])
 
   .controller('MainCtrl', function ($scope) {
+
+    $scope.images = [];
+
+    $scope.loadImages = function () {
+      for (var i = 0; i < 12; i++) {
+        $scope.images.push({id: i, src: "img/placehold100x100.png"})
+      }
+    };
+
   });
